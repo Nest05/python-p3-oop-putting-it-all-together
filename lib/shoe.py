faf8077ca5fc3246910351525f4eb5e1ba6e5b10
Nestor Masinde <nestormasinde@gmail.com>
2024-03-02 21:15:11 +0300

    WIP: oop putting it all together

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        if not isinstance(size, int):
+            raise ValueError("size must be an integer")
+        self.size = size
+        self.condition = "Used"
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
